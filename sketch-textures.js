85f9e4c64716f6d93fa9f8d6dbf4d692d7dd5b34
Geraint <luffgd@gmail.com>
2014-09-05 18:43:49 +0100

    Update settings UI

--- sketch-textures.js
+++ sketch-textures.js
@@ -11,8 +11,11 @@ var sketchTexture = function (canvas, options, callback) {
 	var scale = Math.sqrt(area);
 	var lineLength = options.lineLength;
 	var expectedLineLength = lineLength;
-	if (!options.wrap) {
-		expectedLineLength = Math.min(expectedLineLength, Math.abs(width/diffX), Math.abs(height/diffY));
+	if (!options.wrap.x) {
+		expectedLineLength = Math.min(expectedLineLength, Math.abs(width/diffX));
+	}
+	if (!options.wrap.y) {
+		expectedLineLength = Math.min(expectedLineLength, Math.abs(height/diffY));
 	}
 	
 	var lineCount = area*options.density/options.opacity/options.lineWidth/expectedLineLength;
@@ -22,9 +25,13 @@ var sketchTexture = function (canvas, options, callback) {
 		var midY = Math.random()*height;
 		
 		var forwardLength = lineLength*0.5, backwardLength = lineLength*0.5;
-		if (!options.wrap) {
-			forwardLength = Math.min(forwardLength, (width - midX)/diffX, (0 - midY)/diffY);
-			backwardLength = Math.min(backwardLength, midX/diffX, (midY - height)/diffY);
+		if (!options.wrap.x) {
+			forwardLength = Math.min(forwardLength, (width - midX)/diffX);
+			backwardLength = Math.min(backwardLength, midX/diffX);
+		}
+		if (!options.wrap.y) {
+			forwardLength = Math.min(forwardLength, (0 - midY)/diffY);
+			backwardLength = Math.min(backwardLength, (midY - height)/diffY);
 		}
 		if (options.taper) {
 			forwardLength *= Math.pow(Math.random(), options.taper);
@@ -41,24 +48,19 @@ var sketchTexture = function (canvas, options, callback) {
 		context.globalAlpha = options.opacity;
 		context.beginPath();
 		
-		if (options.wrap) {
-			while (startX < 0 || endX < 0) {
-				startX += width;
-				endX += width;
+		while (startX < 0 || endX < 0) {
+			startX += width;
+			endX += width;
+		}
+		while (startY < 0 || endY < 0) {
+			startY += height;
+			endY += height;
+		}
+		for (var offsetX = 0; endX + offsetX > 0 || startX + offsetX > 0; offsetX -= width) {
+			for (var offsetY = 0; endY + offsetY > 0 || startY + offsetY > 0; offsetY -= height) {
+				context.moveTo(startX + offsetX, startY + offsetY);
+				context.lineTo(endX + offsetX, endY + offsetY);
 			}
-			while (startY < 0 || endY < 0) {
-				startY += height;
-				endY += height;
-			}
-			for (var offsetX = 0; endX + offsetX > 0 || startX + offsetX > 0; offsetX -= width) {
-				for (var offsetY = 0; endY + offsetY > 0 || startY + offsetY > 0; offsetY -= height) {
-					context.moveTo(startX + offsetX, startY + offsetY);
-					context.lineTo(endX + offsetX, endY + offsetY);
-				}
-			}
-		} else {
-			context.moveTo(startX, startY);
-			context.lineTo(endX, endY);
 		}
 
 		context.stroke();
